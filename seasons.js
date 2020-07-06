6be2a43da5561c5666170228055ddf81c278eaae
«julieSalha» <julie.salha@gmail.com>
2020-07-06 09:48:40 +0200

    update custom seasons

--- seasons.js
+++ seasons.js
@@ -1,32 +1,31 @@
   var currentDate = new Date();
   // var currentDate = new Date(2018, 8, 26);
   var years = currentDate.getFullYear();
-  var PrintempsMinDate = new Date(years, 2, 21);
-  var PrintempsMaxDate =  new Date(years, 5, 22);
-  var EteMinDate = new Date(years, 5, 22);
-  var EteMaxDate =  new Date(years, 8, 23);
-  var AutomneMinDate = new Date(years, 8, 23);
-  var AutomneMaxDate =  new Date(years, 11, 21);
-  var HiverMinDateOne = new Date(years, 11, 21);
-  var HiverMaxDateOne =  new Date(years, 11, 31);
-  var HiverMinDateTwo = new Date(years, 0, 1);
-  var HiverMaxDateTwo =  new Date(years, 2, 21);
-  $('#Bienvenue').removeClass('printemps ete automne hiver');
-  $('.background-site').removeClass('printemps ete automne hiver');
-  $('body.personalLogin').removeClass('printemps ete automne hiver');
-  if (currentDate >= PrintempsMinDate && currentDate < PrintempsMaxDate ){
-      $('#Bienvenue').addClass('printemps');
-    
+
+  const PrintempsMinDate = new Date(years, 2, 21);
+  const PrintempsMaxDate =  new Date(years, 5, 22);
+  const EteMinDate = new Date(years, 5, 22);
+  const EteMaxDate =  new Date(years, 8, 23);
+  const AutomneMinDate = new Date(years, 8, 23);
+  const AutomneMaxDate =  new Date(years, 11, 21);
+  const HiverMinDateOne = new Date(years, 11, 21);
+  const HiverMaxDateOne =  new Date(years, 11, 31);
+  const HiverMinDateTwo = new Date(years, 0, 1);
+  const HiverMaxDateTwo =  new Date(years, 2, 21);
+
+  const body = document.querySelector('body');
+  
+  body.classList.remove('body--spring', 'body--summer', 'body--autumn', 'body--winter');
+
+  if (currentDate >= PrintempsMinDate && currentDate < PrintempsMaxDate ) {
+      body.classList.add('body--spring'); 
   }
-  else if(currentDate >= EteMinDate && currentDate < EteMaxDate ){
-      $('#Bienvenue').addClass('ete');
-   
+  else if(currentDate >= EteMinDate && currentDate < EteMaxDate ) {
+      body.classList.add('body--summer'); 
   }
-  else if(currentDate >= AutomneMinDate && currentDate < AutomneMaxDate ){
-      $('#Bienvenue').addClass('automne');
- 
+  else if(currentDate >= AutomneMinDate && currentDate < AutomneMaxDate ) {
+      body.classList.add('body--autumn');
   }
-  else if( (currentDate >= HiverMinDateOne && currentDate <= HiverMaxDateOne) || (currentDate >= HiverMinDateTwo && currentDate <= HiverMaxDateTwo) ){
-      $('#Bienvenue').addClass('hiver');
-     
+  else if( (currentDate >= HiverMinDateOne && currentDate <= HiverMaxDateOne) || (currentDate >= HiverMinDateTwo && currentDate <= HiverMaxDateTwo) ) {
+      body.classList.add('body--winter');
   }
